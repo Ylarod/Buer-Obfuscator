4226db2616d93f7471b5e3774bcbada2e8b390cf
Ylarod <me@ylarod.cn>
2022-05-25 16:46:41 +0800

    port IndirectGlobalVariable to new pass
    Change-Id: I8f5c43fc11ba4241fa38308bbf9f1b8d559fc2d3

--- llvm/include/llvm/Transforms/Obfuscation/IndirectGlobalVariable.h
+++ llvm/include/llvm/Transforms/Obfuscation/IndirectGlobalVariable.h
@@ -1,16 +1,37 @@
 #ifndef OBFUSCATION_INDIRECT_GLOBAL_VARIABLE_H
 #define OBFUSCATION_INDIRECT_GLOBAL_VARIABLE_H
 
+#include "llvm/Pass.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
+#include "llvm/Transforms/Obfuscation/CryptoUtils.h"
+#include "llvm/Transforms/Obfuscation/IPObfuscationContext.h"
+#include "llvm/Transforms/Obfuscation/ObfuscationOptions.h"
+
 // Namespace
 namespace llvm {
-class FunctionPass;
-class PassRegistry;
-struct IPObfuscationContext;
-struct ObfuscationOptions;
 
-FunctionPass* createIndirectGlobalVariablePass();
-FunctionPass* createIndirectGlobalVariablePass(bool flag, IPObfuscationContext *IPO, ObfuscationOptions *Options);
-void initializeIndirectGlobalVariablePass(PassRegistry &Registry);
+struct IndirectGlobalVariable : public PassInfoMixin<IndirectGlobalVariable> {
+  bool enable;
+
+  IPObfuscationContext *IPO;
+  ObfuscationOptions *Options;
+  std::map<GlobalVariable *, unsigned> GVNumbering;
+  std::vector<GlobalVariable *> GlobalVariables;
+
+  IndirectGlobalVariable(bool enable, IPObfuscationContext *IPO, ObfuscationOptions *Options){
+    this->enable = enable;
+    this->IPO = IPO;
+    this->Options = Options;
+  }
+
+  void NumberGlobalVariable(Function &F);
+
+  GlobalVariable *getIndirectGlobalVariables(Function &F, ConstantInt *EncKey);
+
+  PreservedAnalyses run(Function &F, FunctionAnalysisManager &AM);
+
+};
 
 }
 
