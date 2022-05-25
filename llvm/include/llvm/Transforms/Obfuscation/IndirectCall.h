e6b91f950616d83df418ff03c8f7eed88656abc1
Ylarod <me@ylarod.cn>
2022-05-25 16:58:49 +0800

    port IndirectCall to new pass
    Change-Id: Ib50a626cbe0bb8ba6cab3dfe88b91fc1bfc5da86

--- llvm/include/llvm/Transforms/Obfuscation/IndirectCall.h
+++ llvm/include/llvm/Transforms/Obfuscation/IndirectCall.h
@@ -1,17 +1,37 @@
 #ifndef OBFUSCATION_INDIRECT_CALL_H
 #define OBFUSCATION_INDIRECT_CALL_H
 
+#include "llvm/Pass.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
+#include "llvm/Transforms/Obfuscation/CryptoUtils.h"
+#include "llvm/Transforms/Obfuscation/IPObfuscationContext.h"
+#include "llvm/Transforms/Obfuscation/ObfuscationOptions.h"
+
 // Namespace
 namespace llvm {
-class FunctionPass;
-class PassRegistry;
-struct IPObfuscationContext;
-struct ObfuscationOptions;
+struct IndirectCall : public PassInfoMixin<IndirectCall> {
+  bool enable;
 
-FunctionPass* createIndirectCallPass();
-FunctionPass* createIndirectCallPass(bool flag, IPObfuscationContext *IPO, ObfuscationOptions *Options);
-void initializeIndirectCallPass(PassRegistry &Registry);
+  IPObfuscationContext *IPO;
+  ObfuscationOptions *Options;
+  std::map<Function *, unsigned> CalleeNumbering;
+  std::vector<CallInst *> CallSites;
+  std::vector<Function *> Callees;
 
+  IndirectCall(bool enable, IPObfuscationContext *IPO, ObfuscationOptions *Options) {
+    this->enable = enable;
+    this->IPO = IPO;
+    this->Options = Options;
+  }
+
+  void NumberCallees(Function &F);
+
+  GlobalVariable *getIndirectCallees(Function &F, ConstantInt *EncKey);
+
+  PreservedAnalyses run(Function &F, FunctionAnalysisManager &AM);
+
+};
 }
 
 #endif
--- llvm/lib/Transforms/Obfuscation/IndirectCall.cpp
+++ llvm/lib/Transforms/Obfuscation/IndirectCall.cpp
@@ -16,178 +16,140 @@
 #define DEBUG_TYPE "icall"
 
 using namespace llvm;
-namespace {
-struct IndirectCall : public FunctionPass {
-  static char ID;
-  bool flag;
 
-  IPObfuscationContext *IPO;
-  ObfuscationOptions *Options;
-  std::map<Function *, unsigned> CalleeNumbering;
-  std::vector<CallInst *> CallSites;
-  std::vector<Function *> Callees;
-  CryptoUtils RandomEngine;
-  IndirectCall() : FunctionPass(ID) {
-    this->flag = false;
-    IPO = nullptr;
-    this->Options = nullptr;
-  }
-
-  IndirectCall(bool flag, IPObfuscationContext *IPO, ObfuscationOptions *Options) : FunctionPass(ID) {
-    this->flag = flag;
-    this->IPO = IPO;
-    this->Options = Options;
-  }
-
-  StringRef getPassName() const override { return {"IndirectCall"}; }
-
-  void NumberCallees(Function &F) {
-    for (auto &BB:F) {
-      for (auto &I:BB) {
-        if (isa<CallInst>(&I)) {
-          auto *CI = cast<CallInst>(&I);
-          Function *Callee = CI->getCalledFunction();
-          if (Callee == nullptr) {
-            continue;
-          }
-          if (Callee->isIntrinsic()) {
-            continue;
-          }
-          CallSites.push_back(CI);
-          if (CalleeNumbering.count(Callee) == 0) {
-            CalleeNumbering[Callee] = Callees.size();
-            Callees.push_back(Callee);
-          }
+void IndirectCall::NumberCallees(Function &F) {
+  for (auto &BB:F) {
+    for (auto &I:BB) {
+      if (isa<CallInst>(&I)) {
+        auto *CI = cast<CallInst>(&I);
+        Function *Callee = CI->getCalledFunction();
+        if (Callee == nullptr) {
+          continue;
+        }
+        if (Callee->isIntrinsic()) {
+          continue;
+        }
+        CallSites.push_back(CI);
+        if (CalleeNumbering.count(Callee) == 0) {
+          CalleeNumbering[Callee] = Callees.size();
+          Callees.push_back(Callee);
         }
       }
     }
   }
-
-  GlobalVariable *getIndirectCallees(Function &F, ConstantInt *EncKey) {
-    std::string GVName(F.getName().str() + "_IndirectCallees");
-    GlobalVariable *GV = F.getParent()->getNamedGlobal(GVName);
-    if (GV)
-      return GV;
-
-    // callee's address
-    std::vector<Constant *> Elements;
-    for (auto Callee:Callees) {
-      Constant *CE = ConstantExpr::getBitCast(Callee, Type::getInt8PtrTy(F.getContext()));
-      CE = ConstantExpr::getGetElementPtr(Type::getInt8Ty(F.getContext()), CE, EncKey);
-      Elements.push_back(CE);
-    }
-
-    ArrayType *ATy = ArrayType::get(Type::getInt8PtrTy(F.getContext()), Elements.size());
-    Constant *CA = ConstantArray::get(ATy, ArrayRef<Constant *>(Elements));
-    GV = new GlobalVariable(*F.getParent(), ATy, false, GlobalValue::LinkageTypes::PrivateLinkage,
-                                               CA, GVName);
-    appendToCompilerUsed(*F.getParent(), {GV});
+}
+GlobalVariable *IndirectCall::getIndirectCallees(Function &F,
+                                                       ConstantInt *EncKey) {
+  std::string GVName(F.getName().str() + "_IndirectCallees");
+  GlobalVariable *GV = F.getParent()->getNamedGlobal(GVName);
+  if (GV)
     return GV;
+
+  // callee's address
+  std::vector<Constant *> Elements;
+  for (auto Callee:Callees) {
+    Constant *CE = ConstantExpr::getBitCast(Callee, Type::getInt8PtrTy(F.getContext()));
+    CE = ConstantExpr::getGetElementPtr(Type::getInt8Ty(F.getContext()), CE, EncKey);
+    Elements.push_back(CE);
   }
 
-
-  bool runOnFunction(Function &Fn) override {
-    if (!toObfuscate(flag, &Fn, "icall")) {
-      return false;
-    }
-
-    if (Options && Options->skipFunction(Fn.getName())) {
-      return false;
-    }
-
-    LLVMContext &Ctx = Fn.getContext();
-
-    CalleeNumbering.clear();
-    Callees.clear();
-    CallSites.clear();
-
-    NumberCallees(Fn);
-
-    if (Callees.empty()) {
-      return false;
-    }
-
-    uint32_t V = RandomEngine.get_uint32_t() & ~3;
-    ConstantInt *EncKey = ConstantInt::get(Type::getInt32Ty(Ctx), V, false);
-
-    const IPObfuscationContext::IPOInfo *SecretInfo = nullptr;
-    if (IPO) {
-      SecretInfo = IPO->getIPOInfo(&Fn);
-    }
-
-    Value *MySecret;
-    if (SecretInfo) {
-      MySecret = SecretInfo->SecretLI;
-    } else {
-      MySecret = ConstantInt::get(Type::getInt32Ty(Ctx), 0, true);
-    }
-
-    ConstantInt *Zero = ConstantInt::get(Type::getInt32Ty(Ctx), 0);
-    GlobalVariable *Targets = getIndirectCallees(Fn, EncKey);
-
-    for (auto *CI : CallSites) {
-      SmallVector<Value *, 8> Args;
-      SmallVector<AttributeSet, 8> ArgAttrVec;
-
-      Instruction *Call = CI;
-      Function *Callee = CI->getCalledFunction();
-      FunctionType *FTy = CI->getFunctionType();
-      IRBuilder<> IRB(Call);
-
-      Args.clear();
-      ArgAttrVec.clear();
-
-      Value *Idx = ConstantInt::get(Type::getInt32Ty(Ctx), CalleeNumbering[CI->getCalledFunction()]);
-      Value *GEP = IRB.CreateGEP(Targets, {Zero, Idx});
-      LoadInst *EncDestAddr = IRB.CreateLoad(GEP, CI->getName());
-      Constant *X;
-      if (SecretInfo) {
-        X = ConstantExpr::getSub(SecretInfo->SecretCI, EncKey);
-      } else {
-        X = ConstantExpr::getSub(Zero, EncKey);
-      }
-
-      const AttributeList &CallPAL = CI->getAttributes();
-      auto* I = CI->arg_begin();
-      unsigned i = 0;
-
-      for (unsigned e = FTy->getNumParams(); i != e; ++I, ++i) {
-        Args.push_back(*I);
-        AttributeSet Attrs = CallPAL.getParamAttrs(i);
-        ArgAttrVec.push_back(Attrs);
-      }
-
-      for (auto* E = CI->arg_end(); I != E; ++I, ++i) {
-        Args.push_back(*I);
-        ArgAttrVec.push_back(CallPAL.getParamAttrs(i));
-      }
-
-      AttributeList NewCallPAL = AttributeList::get(
-          IRB.getContext(), CallPAL.getFnAttrs(), CallPAL.getRetAttrs(), ArgAttrVec);
-
-      Value *Secret = IRB.CreateSub(X, MySecret);
-      Value *DestAddr = IRB.CreateGEP(EncDestAddr, Secret);
-
-      Value *FnPtr = IRB.CreateBitCast(DestAddr, FTy->getPointerTo());
-      FnPtr->setName("Call_" + Callee->getName());
-      CallInst *NewCall = IRB.CreateCall(FTy, FnPtr, Args, Call->getName());
-      NewCall->setAttributes(NewCallPAL);
-      Call->replaceAllUsesWith(NewCall);
-      Call->eraseFromParent();
-    }
-
-    return true;
-  }
-
-};
-} // namespace llvm
-
-char IndirectCall::ID = 0;
-FunctionPass *llvm::createIndirectCallPass() { return new IndirectCall(); }
-FunctionPass *llvm::createIndirectCallPass(bool flag,
-                                             IPObfuscationContext *IPO,
-                                             ObfuscationOptions *Options) {
-  return new IndirectCall(flag, IPO, Options);
+  ArrayType *ATy = ArrayType::get(Type::getInt8PtrTy(F.getContext()), Elements.size());
+  Constant *CA = ConstantArray::get(ATy, ArrayRef<Constant *>(Elements));
+  GV = new GlobalVariable(*F.getParent(), ATy, false, GlobalValue::LinkageTypes::PrivateLinkage,
+                          CA, GVName);
+  appendToCompilerUsed(*F.getParent(), {GV});
+  return GV;
 }
 
-INITIALIZE_PASS(IndirectCall, "icall", "Enable IR Indirect Call Obfuscation", false, false)
+PreservedAnalyses IndirectCall::run(Function &F, FunctionAnalysisManager &AM) {
+  if (!toObfuscate(enable, &F, "icall")) {
+    return PreservedAnalyses::all();
+  }
+
+  if (Options && Options->skipFunction(F.getName())) {
+    return PreservedAnalyses::all();
+  }
+
+  LLVMContext &Ctx = F.getContext();
+
+  CalleeNumbering.clear();
+  Callees.clear();
+  CallSites.clear();
+
+  NumberCallees(F);
+
+  if (Callees.empty()) {
+    return PreservedAnalyses::all();
+  }
+
+  uint32_t V = IPO->RandomEngine.get_uint32_t() & ~3;
+  ConstantInt *EncKey = ConstantInt::get(Type::getInt32Ty(Ctx), V, false);
+
+  const IPObfuscationContext::IPOInfo *SecretInfo = nullptr;
+  if (IPO) {
+    SecretInfo = IPO->getIPOInfo(&F);
+  }
+
+  Value *MySecret;
+  if (SecretInfo) {
+    MySecret = SecretInfo->SecretLI;
+  } else {
+    MySecret = ConstantInt::get(Type::getInt32Ty(Ctx), 0, true);
+  }
+
+  ConstantInt *Zero = ConstantInt::get(Type::getInt32Ty(Ctx), 0);
+  GlobalVariable *Targets = getIndirectCallees(F, EncKey);
+
+  for (auto *CI : CallSites) {
+    SmallVector<Value *, 8> Args;
+    SmallVector<AttributeSet, 8> ArgAttrVec;
+
+    Instruction *Call = CI;
+    Function *Callee = CI->getCalledFunction();
+    FunctionType *FTy = CI->getFunctionType();
+    IRBuilder<> IRB(Call);
+
+    Args.clear();
+    ArgAttrVec.clear();
+
+    Value *Idx = ConstantInt::get(Type::getInt32Ty(Ctx), CalleeNumbering[CI->getCalledFunction()]);
+    Value *GEP = IRB.CreateGEP(Targets->getType()->getScalarType()->getPointerElementType(),Targets, {Zero, Idx});
+    LoadInst *EncDestAddr = IRB.CreateLoad(GEP->getType()->getPointerElementType(),GEP, CI->getName());
+    Constant *X;
+    if (SecretInfo) {
+      X = ConstantExpr::getSub(SecretInfo->SecretCI, EncKey);
+    } else {
+      X = ConstantExpr::getSub(Zero, EncKey);
+    }
+
+    const AttributeList &CallPAL = CI->getAttributes();
+    auto* I = CI->arg_begin();
+    unsigned i = 0;
+
+    for (unsigned e = FTy->getNumParams(); i != e; ++I, ++i) {
+      Args.push_back(*I);
+      AttributeSet Attrs = CallPAL.getParamAttrs(i);
+      ArgAttrVec.push_back(Attrs);
+    }
+
+    for (auto* E = CI->arg_end(); I != E; ++I, ++i) {
+      Args.push_back(*I);
+      ArgAttrVec.push_back(CallPAL.getParamAttrs(i));
+    }
+
+    AttributeList NewCallPAL = AttributeList::get(
+        IRB.getContext(), CallPAL.getFnAttrs(), CallPAL.getRetAttrs(), ArgAttrVec);
+
+    Value *Secret = IRB.CreateSub(X, MySecret);
+    Value *DestAddr = IRB.CreateGEP(EncDestAddr->getType()->getScalarType()->getPointerElementType(),EncDestAddr, Secret);
+
+    Value *FnPtr = IRB.CreateBitCast(DestAddr, FTy->getPointerTo());
+    FnPtr->setName("Call_" + Callee->getName());
+    CallInst *NewCall = IRB.CreateCall(FTy, FnPtr, Args, Call->getName());
+    NewCall->setAttributes(NewCallPAL);
+    Call->replaceAllUsesWith(NewCall);
+    Call->eraseFromParent();
+  }
+
+  return PreservedAnalyses::none();
+}
