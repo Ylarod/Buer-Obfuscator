04a317f57a621d3a28bc56e01b73d34472bb6801
Ylarod <me@ylarod.cn>
2022-05-25 16:52:38 +0800

    port IndirectBranch to new pass
    Change-Id: I2f8bbf0669775473abe7bf45c9103b0d258fe7f6

--- llvm/include/llvm/Transforms/Obfuscation/IndirectBranch.h
+++ llvm/include/llvm/Transforms/Obfuscation/IndirectBranch.h
@@ -1,16 +1,37 @@
 #ifndef OBFUSCATION_INDIRECTBR_H
 #define OBFUSCATION_INDIRECTBR_H
 
+#include "llvm/Pass.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
+#include "llvm/Transforms/Obfuscation/CryptoUtils.h"
+#include "llvm/Transforms/Obfuscation/IPObfuscationContext.h"
+#include "llvm/Transforms/Obfuscation/ObfuscationOptions.h"
+
 // Namespace
 namespace llvm {
-class FunctionPass;
-class PassRegistry;
-struct IPObfuscationContext;
-struct ObfuscationOptions;
 
-FunctionPass* createIndirectBranchPass();
-FunctionPass* createIndirectBranchPass(bool flag, IPObfuscationContext *IPO, ObfuscationOptions *Options);
-void initializeIndirectBranchPass(PassRegistry &Registry);
+struct IndirectBranch : public PassInfoMixin<IndirectBranch> {
+  bool enable;
+
+  IPObfuscationContext *IPO;
+  ObfuscationOptions *Options;
+  std::map<BasicBlock *, unsigned> BBNumbering;
+  std::vector<BasicBlock *> BBTargets;        //all conditional branch targets
+
+  IndirectBranch(bool flag, IPObfuscationContext *IPO, ObfuscationOptions *Options) {
+    this->enable = flag;
+    this->IPO = IPO;
+    this->Options = Options;
+  }
+
+  void NumberBasicBlock(Function &F);
+
+  GlobalVariable *getIndirectTargets(Function &F, ConstantInt *EncKey);
+
+  PreservedAnalyses run(Function &F, FunctionAnalysisManager &AM);
+
+};
 
 }
 
